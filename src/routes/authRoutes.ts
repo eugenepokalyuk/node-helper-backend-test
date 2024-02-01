05fcf8c27eb41536b66f81a2ee68df92bd11e188
Evgenii <eugene.pokalyuk@gmail.com>
2024-02-02 04:24:05 +0700

    Update authRoutes.ts

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,5 +1,5 @@
-const express = require('express');
-const authController = require('../controllers/authController');
+import express from 'express';
+import * as authController from '../controllers/authController';
 
 const router = express.Router();
 
@@ -7,4 +7,4 @@ router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', authController.logout);
 
-module.exports = router;
+export default router;
